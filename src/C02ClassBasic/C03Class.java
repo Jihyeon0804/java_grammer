08469b97d0892b12a537240fd2e24b1e4a887f21
Jihyeon0804 <cam080421@naver.com>
2025-06-18 11:37:04 +0900

    Class

--- src/C02ClassBasic/C03Class.java
+++ src/C02ClassBasic/C03Class.java
@@ -1,5 +1,7 @@
 package C02ClassBasic;
 
+import java.util.*;
+
 public class C03Class {
 
     public static void main(String[] args) {
@@ -34,15 +36,32 @@ public class C03Class {
         // A 부서의 매출 : 10, 20, ...
         MyCalculatorInstance mi1 = new MyCalculatorInstance();
         // MyCalCulatorInstance.sumAcc() : static이 없으므로 sumAcc는 호출이 불가 -> 객체로 만들어서 호출해야 함
-        System.out.println(mi1.sumAcc(10));
-        System.out.println(mi1.sumAcc(20));
+        mi1.sumAcc(10);
+        System.out.println(mi1.total);
+        mi1.sumAcc(20);
+        System.out.println(mi1.total);
 
 
         // B 부서의 매출 : 10, 20, ...
         MyCalculatorInstance mi2 = new MyCalculatorInstance();
         // MyCalculatorInstance.sumAcc() : static이 없으므로 sumAcc는 호출이 불가 -> 객체로 만들어서 호출해야 함
-        System.out.println(mi2.sumAcc(10));
-        System.out.println(mi2.sumAcc(20));
+        mi2.sumAcc(10);
+        System.out.println(mi2.total);
+        mi2.sumAcc(20);
+        System.out.println(mi2.total);
+
+        System.out.println(MyCalculatorInstance.total_count);
+        
+        // 자료 구조에서 객체 사용 예시
+        // 1) 배열
+        String[] arr1 = {"hello", "java", "python"};
+        Arrays.sort(arr1);                              // 클래스명.메서드명() : 클래스 메서드
+        System.out.println(arr1.length);                // 객체(인스턴스) 메서드
+        
+        // 2) 리스트
+        List<String> myList = new ArrayList<>();
+        myList.sort(Comparator.naturalOrder());     // 객체 메서드
+        Collections.sort(myList);                   // 클래스 메서드
     }
 
 }
@@ -63,14 +82,26 @@ class MyCalculator {
 
 class MyCalculatorInstance {
     int total = 0;
-
-    public int sum(int a, int b) {
+    
+    // 클래스 변수를 모든 인스턴스(객체) 간 상태를 공유 목적으로 주로 사용
+    static int total_count = 0;
+    
+    // 클래스를 만들면, 아래와 같은 클래스명의 이름을 가진 메서드(생성자)가 기본으로 만들어짐
+    // 생성자의 주된 목적은 객체 생성 시 실행되면서 객체 변수를 초기화하기 위해 주로 사용
+    public MyCalculatorInstance() {
+        total_count++;
+    }
+    
+    // 클래스 메서드는 "객체의 상태와 상관없는" 독립적인 로직 수행 시에 사용 가능
+    public static int sum(int a, int b) {
         return a + b;
     }
 
-    public int sumAcc(int a) {
-        total += a;
-        return total;
+    public void sumAcc(int total) {
+        // this 는 객체 그 자신을 의미
+        // 일반적으로 매개변수와 객체변수를 구분짓기 위해 사용
+        // ex) 매개 변수명도 total, 객체 변수명도 total일 경우
+        this.total += total;
     }
 }
 
