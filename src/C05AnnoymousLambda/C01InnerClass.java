711ac00705ed4e4f7c0bc10153bfd81393d62990
Jihyeon0804 <cam080421@naver.com>
2025-06-23 16:41:29 +0900

    Inner Class

---
+++ src/C05AnnoymousLambda/C01InnerClass.java
@@ -0,0 +1,33 @@
+package C05AnnoymousLambda;
+
+public class C01InnerClass {
+    public static void main(String[] args) {
+        MemberInnerClass m1 = new MemberInnerClass();
+        System.out.println(m1.getA());      // 10
+
+        MemberInnerClass.StaticInnerClass ms1 = new MemberInnerClass.StaticInnerClass();
+        ms1.display();                      // 20
+    }
+}
+
+// (일반) 내부 클래스
+class MemberInnerClass {
+    int a;
+
+    MemberInnerClass () {
+        this.a = 10;
+    }
+
+    int getA() {
+        return this.a;
+    }
+
+    // static 내부 클래스 : MemberInnerClass의 static 변수처럼 활용
+    static class StaticInnerClass {
+        int b = 20;
+
+        void display() {
+            System.out.println(this.b);
+        }
+    }
+}
